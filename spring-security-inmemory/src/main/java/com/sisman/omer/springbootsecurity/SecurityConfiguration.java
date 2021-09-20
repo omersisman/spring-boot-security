4eb28c29fe148bbc2e0a75783e647745d6cebe65
omersisman <44743187+omersisman@users.noreply.github.com>
2021-09-20 13:29:25 +0300

    add files via upload

---
+++ spring-security-inmemory/src/main/java/com/sisman/omer/springbootsecurity/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.sisman.omer.springbootsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //Set your configuration on the auth object
+        auth.inMemoryAuthentication()
+                .withUser("omer")
+                .password("faruk")
+                .roles("ADMIN")
+                .and()
+                .withUser("yusuf")
+                .password("tugrul")
+                .roles("USER")
+                .and()
+                .withUser("omer2")
+                .password("unal")
+                .roles("USER");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
